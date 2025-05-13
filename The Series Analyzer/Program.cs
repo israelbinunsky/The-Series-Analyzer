15f42dab5a2afd9fad15a393f0eba5432c1d292a
israelb <israelbinunsky@gmail.com>
2025-05-13 11:18:52 +0300

    menue

--- The Series Analyzer/Program.cs
+++ The Series Analyzer/Program.cs
@@ -20,7 +20,55 @@ namespace The_Series_Analyzer
             }
             return args;
         }
-        
-
+        static void menue()
+        {
+            Console.WriteLine("enter a letter a - j");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "a":
+                    inputSeries();
+                    menue();
+                    break;
+                case "b":
+                    display();
+                    menue();
+                    break;
+                case "c":
+                    reversed();
+                    menue();
+                    break;
+                case "d":
+                    sorted();
+                    menue();
+                    break;
+                case "e":
+                    MaxValue();
+                    menue();
+                    break;
+                case "f":
+                    MinValue();
+                    menue();
+                    break;
+                case "g":
+                    Average();
+                    menue();
+                    break;
+                case "h":
+                    numElements();
+                    menue();
+                    break;
+                case "i":
+                    sum();
+                    menue();
+                    break;
+                case "j":
+                    break;
+                default:
+                    Console.WriteLine("invalid syntax");
+                    menue();
+                    break;
+            }
+        }
     }
 }
